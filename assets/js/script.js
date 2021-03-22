e4ad92bac95cb90e22ce1057a5f89dd4d81f6cfb
Richard Hopkins <richard.t.hopkins@outlook.com>
2021-03-23 01:43:12 +1100

    added reset.css and updated the style.css created array of questions.

---
+++ assets/js/script.js
@@ -0,0 +1,92 @@
+//array of question objects.
+var questions = [
+    //question 1
+    {
+        question: "",
+        answerA: "",
+        answerB: "",
+        answerC: "",
+        andswerD: ""
+    },
+    //question 2
+    {
+        question: "",
+        answerA: "",
+        answerB: "",
+        answerC: "",
+        andswerD: ""
+    },
+    //question 3
+    {
+         question: "",
+        answerA: "",
+        answerB: "",
+        answerC: "",
+        andswerD: ""
+    },
+    //question 4
+    {
+        question: "",
+        answerA: "",
+        answerB: "",
+        answerC: "",
+        andswerD: ""
+    },
+    //question 5
+    {
+        question: "",
+        answerA: "",
+        answerB: "",
+        answerC: "",
+        andswerD: ""
+    }
+];
+
+
+//function for running the quiz
+
+//function to check if answer is correct or incorrect.
+
+//function for randomly selecting the question and answer order.
+
+//function for count down timer
+
+//button click events. for start button high score button and question buttons.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
