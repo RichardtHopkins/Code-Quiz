e9d01474e7bbd4a1876d41a71bd09494e038c113
Richard Hopkins <richard.t.hopkins@outlook.com>
2021-03-26 01:21:09 +1100

    Added in questions and answers. got start button working. added in place holders for questions and answers.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,68 +2,117 @@
 var questions = [
     //question 1
     {
-        question: "",
-        answerA: "",
-        answerB: "",
-        answerC: "",
-        andswerD: ""
+        question: "Which of the following is a valid variable name?",
+        A: "!variable",
+        B: "8digits",
+        C: "_myVar",
+        D: "123",
+        answer: "C"
     },
     //question 2
     {
-        question: "",
-        answerA: "",
-        answerB: "",
-        answerC: "",
-        andswerD: ""
+        question: "What symbol is used to assign a statement to a variable?",
+        A: "+",
+        B: ">",
+        C: "#",
+        D: "=",
+        answer: "D"
     },
     //question 3
     {
-         question: "",
-        answerA: "",
-        answerB: "",
-        answerC: "",
-        andswerD: ""
+         question: "Which of these is not a primitave data type?",
+         A: "var",
+         B: "number",
+         C: "boolean",
+         D: "undefined",
+         answer: "A"
     },
     //question 4
     {
-        question: "",
-        answerA: "",
-        answerB: "",
-        answerC: "",
-        andswerD: ""
+        question: "Which of the following is the correct syntax for the the while loop?",
+        A: "while (statement) condition;",
+        B: "while (condition) statement;",
+        C: "while (initial expression | condition | increment) statement;",
+        D: "while (condition) {statement}else{statement};",
+        answer: "B"
     },
     //question 5
     {
-        question: "",
-        answerA: "",
-        answerB: "",
-        answerC: "",
-        andswerD: ""
+        question: "How many primitive types does javascript have?",
+        A: "5",
+        B: "11",
+        C: "7",
+        D: "Too many!",
+        answer: "C"
     }
 ];
 
-
 var timeE1 = document.getElementById('timer');
 var mainElement = document.getElementById('main-card');
+
 var secondsLeft = 60;
+var startCountDown = 4;
+
 var startButton = document.createElement('button');
+
 startButton.setAttribute('class', 'start-button');
+
 startButton.textContent = "START";
+
 mainElement.appendChild(startButton);
 
-// starts the timer when called
-function updateSeconds(){
+// starts the  when called
+function gameTimer(){
     var timer = setInterval(function(){
         secondsLeft--;
         timeE1.textContent = secondsLeft;
         if(secondsLeft === 0){
+            timeE1.textContent = "";
             clearInterval(timer);
         }
     }, 1000);
 };
 
+var startCountDown = 4;
+//start count down.
+function gameStartCounter(){
+    var timer = setInterval(function(){
+        startCountDown--;
+        mainElement.textContent = startCountDown;
+        if(startCountDown === 0){
+            mainElement.textContent = "";
+            clearInterval(timer);
+            gameTimer();
+            startGame();
+        }
+    }, 1000);
+};
 
-updateSeconds();
+//start game function
+function startGame(){
+    
+    
+    console.log("started")
+
+
+}
+
+
+//end game function
+function endGame(){
+
+
+
+}
+
+//when the start button is pressed remove the start button and start the game.
+startButton.addEventListener("click", function(event){
+    event.preventDefault();
+    startButton.setAttribute("style", "display: none;");
+    gameStartCounter();
+});
+
+
 
 
 
